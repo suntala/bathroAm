af650cd54de3aa13bf316766396722ede8ad6cfe
suntala <suntala@hotmail.com>
2017-12-04 15:30:17 +0100

    new setup

--- database-connection.js
+++ database-connection.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-const connectionString = process.env.DB_URL || 'mongodb://localhost/br'
-mongoose.connect(connectionString, { useMongoClient: true })
-
+mongoose.connect('mongodb://localhost/br', { useMongoClient: true })
 
 
 
@@ -10,3 +8,10 @@ mongoose.connect(connectionString, { useMongoClient: true })
 // const mongoose = require('mongoose')
 // mongoose.Promise = global.Promise
 // mongoose.connect('mongodb://localhost/br', { useMongoClient: true })
+
+// AFTER DOCKER
+// const mongoose = require('mongoose')
+// mongoose.Promise = global.Promise
+// const connectionString = process.env.DB_URL || 'mongodb://localhost/br'
+// mongoose.connect(connectionString, { useMongoClient: true })
+
