9876b91d82c54c5aef517801782429d90104f6bb
suntala <suntala@hotmail.com>
2017-12-20 15:41:44 +0100

    Added to input form

--- routes/edit-restaurant.js
+++ routes/edit-restaurant.js
@@ -7,14 +7,95 @@ router.get('/create', (req, res, next) => {
 })
 
 router.post('/create', async (req, res, next) => {
-    let timeFrom = req.body.timeFrom.replace(':','');
-    let timeTo = req.body.timeTo.replace(':','');
-    let rawHours = [{weekday: req.body.openDay, intervals: [{from: timeFrom, to: timeTo}]}]
+    let timeFrom1 = req.body.timeFrom1.replace(':','');
+    let timeTo1 = req.body.timeTo1.replace(':','');
+    let timeFrom2 = req.body.timeFrom2.replace(':','');
+    let timeTo2 = req.body.timeTo2.replace(':','');
+
+    // let test = [timeFrom1, timeFrom2]
+    // console.log(timeFrom2)
+
+    let weekdays = req.body.weekday
+    let rawHours = []
+    for (let i = 0; i < weekdays.length; i++) {
+        let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
+        // if (test.length == 2) {
+        //     let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
+        // }
+        // else {
+        //     let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1}]}
+        // }
+        rawHours.push(day)
+    }
     let resto = await RestoServices.add({ name: req.body.name, status: req.body.status, neighborhood: req.body.neighborhood, openingHours: req.body.openingHours, address: req.body.address, website: req.body.website })
     await RestoServices.inputCoo(resto.id)
     await RestoServices.inputHours(resto.id,rawHours)
     res.redirect('/restaurant/all')
 })
+
+
+
+
+
+// router.post('/create', async (req, res, next) => {
+//     let timeFrom1 = req.body.timeFrom1.replace(':','');
+//     let timeTo1 = req.body.timeTo1.replace(':','');
+//     let timeFrom2 = req.body.timeFrom2.replace(':','');
+//     let timeTo2 = req.body.timeTo2.replace(':','');
+
+//     let test = [timeFrom1, timeFrom2]
+//     console.log(timeFrom2)
+
+//     let weekdays = req.body.weekday
+//     let rawHours = []
+//     for (let i = 0; i < weekdays.length; i++) {
+//         // let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
+//         if (test.length == 2) {
+//             let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
+//         }
+//         else {
+//             let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1}]}
+//         }
+//         rawHours.push(day)
+//     }
+//     let resto = await RestoServices.add({ name: req.body.name, status: req.body.status, neighborhood: req.body.neighborhood, openingHours: req.body.openingHours, address: req.body.address, website: req.body.website })
+//     await RestoServices.inputCoo(resto.id)
+//     await RestoServices.inputHours(resto.id,rawHours)
+//     res.redirect('/restaurant/all')
+// })
+
+
+
+
+
+
+
+// router.post('/create', async (req, res, next) => {
+//     let timeFrom1 = req.body.timeFrom1.replace(':','');
+//     let timeTo1 = req.body.timeTo1.replace(':','');
+//     let timeFrom2 = req.body.timeFrom2.replace(':','');
+//     let timeTo2 = req.body.timeTo2.replace(':','');
+//     let weekdays = 
+//     let rawHours = [{weekday: req.body.weekday, intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}]
+//     let resto = await RestoServices.add({ name: req.body.name, status: req.body.status, neighborhood: req.body.neighborhood, openingHours: req.body.openingHours, address: req.body.address, website: req.body.website })
+//     await RestoServices.inputCoo(resto.id)
+//     await RestoServices.inputHours(resto.id,rawHours)
+//     res.redirect('/restaurant/all')
+// })
+
+
+
+// router.post('/create', async (req, res, next) => {
+//     let timeFrom1 = req.body.timeFrom1.replace(':','');
+//     let timeTo1 = req.body.timeTo1.replace(':','');
+//     let timeFrom2 = req.body.timeFrom2.replace(':','');
+//     let timeTo2 = req.body.timeTo2.replace(':','');
+//     let rawHours = [{weekday: req.body.openDay, intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}]
+//     let resto = await RestoServices.add({ name: req.body.name, status: req.body.status, neighborhood: req.body.neighborhood, openingHours: req.body.openingHours, address: req.body.address, website: req.body.website })
+//     await RestoServices.inputCoo(resto.id)
+//     await RestoServices.inputHours(resto.id,rawHours)
+//     res.redirect('/restaurant/all')
+// })
 
 
 
