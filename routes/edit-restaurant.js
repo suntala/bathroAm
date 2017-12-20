6f3479f1afb33633c946b8141f3c21b9d5cf9092
suntala <suntala@hotmail.com>
2017-12-20 22:50:33 +0100

    Refining the input form

--- routes/edit-restaurant.js
+++ routes/edit-restaurant.js
@@ -12,11 +12,18 @@ router.post('/create', async (req, res, next) => {
     let timeTo1 = req.body.timeTo1.replace(':','');
     let timeFrom2 = req.body.timeFrom2.replace(':','');
     let timeTo2 = req.body.timeTo2.replace(':','');
-    let weekdays = req.body.weekday
+    let weekdays = [req.body.weekday]
     let rawHours = []
     for (let i = 0; i < weekdays.length; i++) {
-        let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
-        rawHours.push(day)
+        if (!req.body.confirmation) {
+            let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
+            rawHours.push(day)
+        }
+        else {
+            let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1}]}
+            rawHours.push(day)
+        }
+        // let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
     }
     let resto = await RestoServices.add({ name: req.body.name, status: req.body.status, neighborhood: req.body.neighborhood, openingHours: req.body.openingHours, address: req.body.address, website: req.body.website })
     await RestoServices.inputCoo(resto.id)
@@ -24,6 +31,23 @@ router.post('/create', async (req, res, next) => {
     res.redirect('/restaurant/all')
 })
 //how come this worked with :  openingHours: req.body.openingHours
+
+// router.post('/create', async (req, res, next) => {
+//     let timeFrom1 = req.body.timeFrom1.replace(':','');
+//     let timeTo1 = req.body.timeTo1.replace(':','');
+//     let timeFrom2 = req.body.timeFrom2.replace(':','');
+//     let timeTo2 = req.body.timeTo2.replace(':','');
+//     let weekdays = req.body.weekday
+//     let rawHours = []
+//     for (let i = 0; i < weekdays.length; i++) {
+//         let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
+//         rawHours.push(day)
+//     }
+//     let resto = await RestoServices.add({ name: req.body.name, status: req.body.status, neighborhood: req.body.neighborhood, openingHours: req.body.openingHours, address: req.body.address, website: req.body.website })
+//     await RestoServices.inputCoo(resto.id)
+//     await RestoServices.inputHours(resto.id,rawHours)
+//     res.redirect('/restaurant/all')
+// })
 
 router.get('/edit', async (req, res, next) => {
     let participants = await RestoServices.alphaRestaurants()
