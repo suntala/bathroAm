7fd0ece3a707eb42f44a7eb784088ad80b3fd75c
suntala <suntala@hotmail.com>
2017-12-20 23:07:40 +0100

    Added a panel to the editing form

--- routes/edit-restaurant.js
+++ routes/edit-restaurant.js
@@ -101,11 +101,19 @@ router.post('/:id/edit', async (req, res, next) => {
     let timeTo1 = req.body.timeTo1.replace(':','');
     let timeFrom2 = req.body.timeFrom2.replace(':','');
     let timeTo2 = req.body.timeTo2.replace(':','');
-    let weekdays = req.body.weekday
+    let weekdays = [req.body.weekday]
     let rawHours = []
     for (let i = 0; i < weekdays.length; i++) {
-        let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
-        rawHours.push(day)
+        // let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
+        // rawHours.push(day)
+        if (!req.body.confirmation) {
+            let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1},{from: timeFrom2, to: timeTo2}]}
+            rawHours.push(day)
+        }
+        else {
+            let day = {weekday: weekdays[i], intervals: [{from: timeFrom1, to: timeTo1}]}
+            rawHours.push(day)
+        }
     }
     let resto = await RestoServices.edit(req.params.id, { name: req.body.name, status: req.body.status, neighborhood: req.body.neighborhood, openingHours: rawHours, address: req.body.address, website: req.body.website})
     await RestoServices.inputCoo(resto.id)
@@ -114,5 +122,7 @@ router.post('/:id/edit', async (req, res, next) => {
 })
 //have less repetition; create a function to take care of time
 //I guess I change all the elements with edit for now
+//take care of null problem here too
 
 module.exports = router
+
